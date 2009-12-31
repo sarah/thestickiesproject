7c901043409f6842eebff366337011ff3a34f5a0
Sarah Gray & Corey Haines <sarahg.gray+coreyhaines@gmail.com>
2009-12-30 22:01:18 -0600

    cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,9 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# config.gem 'the_metric_system'
+# config.gem 'rspec'
+# config.gem 'rspec-rails'
+# config.gem 'cucumber'
+# config.gem 'webrat'
--- public/javascripts/stickies.js
+++ public/javascripts/stickies.js
@@ -1,6 +1,6 @@
+var EDITABLE_STICKY_PROPS = { type : 'textarea', onblur : 'submit', event : "dblclick", tooltip : "Double-click to edit", indicator : 'Saving...', placeholder : "Double-click to edit" };
+
 function update_sticky_text(value, settings) {
-  console.log("value " + value + " settings " + settings + " this " + this);
-  console.log("url " + url );
   var stickie = parent_sticky(this);
   var url = sticky_update_url_for(stickie);
   update_sticky(url, value);
@@ -12,7 +12,7 @@ function update_sticky(url, value){
 }
 
 function hookup_sticky_editing(){
-  $('.sticky .editable').editable(update_sticky_text, { type : 'textarea', onblur : 'submit', event : "dblclick" });
+  $('.sticky .editable').editable(update_sticky_text, EDITABLE_STICKY_PROPS);
 }
 
 function sticky_update_url_for(sticky_div){
