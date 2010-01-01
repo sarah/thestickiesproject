bb6faffd88b524200a9e0925fd5cb2385023f120
Sarah Gray & Corey Haines <sarahg.gray+coreyhaines@gmail.com>
2010-01-01 12:55:51 -0600

    Clean up javascript specs a bit

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -1,1 +1,5 @@
-// Use this file to require common dependencies or to setup useful test functions.
+var rails_require = function rails_require(js_file) {
+  require("../../public/javascripts/" + js_file + ".js");
+}
+rails_require("jquery.jeditable.mini");
+rails_require("jquery-ui-1.7.2.custom.min");
--- spec/javascripts/stickies_spec.js
+++ spec/javascripts/stickies_spec.js
@@ -1,23 +1,31 @@
 require("spec_helper.js");
-require("../../public/javascripts/jquery.jeditable.mini.js");
-require("../../public/javascripts/jquery-ui-1.7.2.custom.min.js");
-require("../../public/javascripts/stickies.js");
+rails_require('stickies');
 
 Screw.Unit(function(){
-  describe("getting information from sticky div", function(){
+
+  describe("#sticky_update_url_for", function(){
+
     it("can get the update url", function(){
       var sticky_div = $("#sticky_23");
       sticky_div.attr('data-update-url', "/stickies/25");
       expect(sticky_update_url_for(sticky_div)).to(equal, "/stickies/25");
     });
   });
-  describe("getting the sticky div", function(){
-    it("can retrieve it from the .editable element", function(){
+
+  describe("#parent_sticky", function(){
+    it("returns the sticky_div for the editable", function(){
       var sticky_editable_div = $("#sticky_23 .editable");
       var sticky_div = parent_sticky(sticky_editable_div);
       expect(sticky_div.attr('id')).to(equal, 'sticky_23');
     });
+
+    it("returns the sticky_div for the delete_sticky_link", function(){
+      var sticky_delete_link = $("#sticky_23 .delete_sticky_link" );
+      var sticky_div = parent_sticky(sticky_delete_link);
+      expect(sticky_div.attr("id")).to(equal, 'sticky_23');
+    });
   });
+
   describe("getting information from the jquery ui object", function(){
         
     it("can get the position", function(){
