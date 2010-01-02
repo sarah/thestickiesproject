43a7bac33f8cce0454d7e9b50309cf715ebc718f
Sarah Gray & Corey Haines <sarahg.gray+coreyhaines@gmail.com>
2010-01-01 20:06:47 -0600

    Switch away from mocks for dom elements
    - Using html fixture to construct dom element, only mock
      concrete javascript objects

--- public/javascripts/tsp.js
+++ public/javascripts/tsp.js
@@ -2,13 +2,14 @@ var TSP = {};
 
 TSP.Handlers = {
   update_sticky_text: function(value, settings){
-    var sticky = TSP.Lookups.get_sticky_parent_for(this);
+    var sticky_div = TSP.Lookups.get_sticky_parent_for(this);
+    var sticky = TSP.Lookups.get_sticky(sticky_div);
     sticky.update_content(value);
     return value;
    }
 };
 TSP.Lookups = {
   get_sticky_parent_for : function(element){
-    return element.parent('.sticky');
+    return element.closest('.sticky');
    }
 };
--- spec/javascripts/tsp_spec.js
+++ spec/javascripts/tsp_spec.js
@@ -1,31 +1,38 @@
 require('spec_helper.js');
 rails_require('tsp');
 
+var Helpers = {};
+
+Helpers.editable_div = function(){
+  return $('.sticky .editable');
+};
+
+var editable_div = Helpers.editable_div;
+
+
 Screw.Unit(function(){
+  var mock_sticky = function(){
+    var sticky = mock();
+    mock(TSP.Lookups).should_receive('get_sticky').and_return(sticky);
+    stub(sticky, 'update_content');
+    return sticky;
+  };
   describe("TSP.Handlers", function(){
     describe("#update_sticky_text", function(){
 
-
-
       it("returns the value passed in", function(){
-        var editable_div = {};
-        var sticky = {};
+        mock_sticky();
 
-        stub_call(TSP.Lookups, 'get_sticky_parent_for', sticky);
-        stub_call(sticky, 'update_content');
-
-        var value = TSP.Handlers.update_sticky_text("new text", null);
+        var value = TSP.Handlers.update_sticky_text.apply(editable_div(), ['new text', null]);
         expect(value).to(equal, "new text");
       });
 
       it("updates the sticky value with the passed value", function(){
-        var editable_div = {};
-        var sticky = mock();
-        stub_call(TSP.Lookups, 'get_sticky_parent_for', sticky);
+        var sticky = mock_sticky();
 
         sticky.should_receive('update_content').with_arguments('new content').exactly(1);
 
-        TSP.Handlers.update_sticky_text.apply(editable_div, ['new content', null]);
+        TSP.Handlers.update_sticky_text.apply(editable_div(), ['new content', null]);
       });
     });
   });
@@ -33,9 +40,12 @@ Screw.Unit(function(){
   describe("TSP.Lookups", function(){
    describe("#get_sticky_parent_for", function(){
     it("retrieves the parent .sticky for the editable tag", function() {
-      var editable_div = $('.sticky .editable');
-      expect(TSP.Lookups.get_sticky_parent_for(editable_div).attr('class')).to(equal, 'sticky');
+      expect(TSP.Lookups.get_sticky_parent_for(editable_div()).attr('class')).to(equal, 'sticky');
     });
   });
  });
+
+  describe("TSP.get_sticky", function(){
+
+    });
 });
