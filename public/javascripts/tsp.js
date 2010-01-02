dd84afa06c1c88bbf0f20060562ebae40ec5c158
Sarah Gray & Corey Haines <sarahg.gray+coreyhaines@gmail.com>
2010-01-02 13:56:16 -0600

    Refactored TSP into an instance, get_tsp();

--- public/javascripts/tsp.js
+++ public/javascripts/tsp.js
@@ -1,17 +1,17 @@
-var TSP = {};
-
-TSP.Handlers = {
-  update_sticky_text: function(value, settings){
-    var sticky = TSP.Lookups.sticky_from(this);
-    sticky.update_content(value);
-    return value;
-   }
-};
-
-TSP.Lookups = {
-  sticky_from: function(element){
-    var sticky = {};
-    sticky.div = element.hasClass('sticky') ? element : element.closest(".sticky");
-    return sticky;
-   }
+var get_tsp = function() {
+  var tsp = {};
+  tsp.lookups = {
+      sticky_from:function(){
+                  
+                    return{update_content:function(){}};
+                  } 
+  };
+  tsp.handlers = {
+    update_sticky_text: function(value, settings){
+                          var sticky = tsp.lookups.sticky_from();
+                          sticky.update_content(value);
+                          return value;
+                        }
+  };
+  return tsp; 
 };
--- spec/javascripts/tsp_spec.js
+++ spec/javascripts/tsp_spec.js
@@ -17,47 +17,47 @@ var get_sticky_div = Helpers.sticky_div;
 
 
 Screw.Unit(function(){
-  var mock_sticky = function(){
+  var mock_sticky = function(tsp){
     var sticky = mock();
-    mock(TSP.Lookups).should_receive('sticky_from').and_return(sticky);
+    stub(tsp.lookups, 'sticky_from').and_return(sticky);
     stub(sticky, 'update_content');
     return sticky;
   };
-  describe("TSP.Handlers", function(){
+  var tsp;
+  before(function(){
+    tsp  = get_tsp();
+    });
+  describe("tsp.handlers", function(){
     describe("#update_sticky_text", function(){
-
+      var sticky;
+      before(function(){
+        sticky = mock_sticky(tsp);
+        });
       it("returns the value passed in", function(){
-        mock_sticky();
-
-        var value = TSP.Handlers.update_sticky_text.apply(get_editable_div(), ['new text', null]);
+        var value = tsp.handlers.update_sticky_text.apply(get_editable_div(), ['new text', null]);
         expect(value).to(equal, "new text");
       });
 
       it("updates the sticky value with the passed value", function(){
-        mock_sticky().should_receive('update_content').with_arguments('new content').exactly(1);
+        sticky.should_receive('update_content').with_arguments('new content').exactly(1);
 
-        TSP.Handlers.update_sticky_text.apply(get_editable_div(), ['new content', null]);
+        tsp.handlers.update_sticky_text.apply(get_editable_div(), ['new content', null]);
       });
     });
   });
 
-  describe("sticky object returned from TSP.sticky_from", function(){
-      it("contains the sticky div it was spawned from", function(){
-        var sticky_div = get_sticky_div();
-        sticky_div.attr('id', 'foo');
+  describe("tsp.lookups", function(){
+    describe("sticky object returned from #sticky_from", function(){
+        var sticky;
+        before(function(){
+          sticky = tsp.lookups.sticky_from();
 
-        var sticky = TSP.Lookups.sticky_from(sticky_div);
-        expect(sticky.div.attr('id')).to(equal, 'foo');
+          });
+        describe("#update_content", function(){
+          it("can be called", function(){
+            sticky.update_content();
+          });
+        });
       });
-
-      it("can generate from a child of the sticky div", function(){
-        var sticky_div = get_sticky_div();
-        sticky_div.attr('id', 'bar');
-
-        var editable_child_div = sticky_div.find('.editable');
-
-        var sticky = TSP.Lookups.sticky_from(editable_child_div);
-        expect(sticky.div.attr('id')).to(equal, 'bar');
-      });
-    });
+  });
 });
