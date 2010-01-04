f6ff6ec9db58d8f1b706ce8b30a0d257e35ebb5c
Corey Haines & Sarah Gray <sarahg.gray+coreyhaines@gmail.com>
2010-01-03 20:58:40 -0600

    three-row structural layout; initial css colors

--- public/javascripts/stickies.js
+++ public/javascripts/stickies.js
@@ -6,7 +6,7 @@ function hookup_sticky_editing(){
 }
 function hookup_draggability(){
   var handler = TSP.get().handlers.update_sticky_position;
-  $('.sticky').draggable({ stop: handler, containment: "#stage", scroll: true  });
+  $('.sticky').draggable({ stop: handler, containment: "#stage" });
 }
 function hookup_sticky_delete(){
   var handler = TSP.get().handlers.destroy_sticky;
