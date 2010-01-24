f131723b16d9458cb69333a33f8beffd425f4d75
Sarah Gray & Corey Haines <sarahg.gray+coreyhaines@gmail.com>
2010-01-24 15:09:08 -0600

    Extract TSP.handlers

--- public/javascripts/tsp.js
+++ public/javascripts/tsp.js
@@ -82,36 +82,10 @@ TSP.get = function() {
       }
   };
 
-  var handlers = {
-    create_sticky: function() {
-                     tsp.builders.create_sticky();
-                   },
-    destroy_sticky: function(){
-                    var sticky = tsp.lookups.sticky_from(this);
-                    sticky.destroy();
-                  },
-    update_sticky_text: function(value, settings){
-                         var sticky = tsp.lookups.sticky_from(this);
-                         sticky.update_content(value);
-                         return value;
-                        },
-    update_sticky_position: function(event, ui){
-                          var left = Math.round(ui.position.left);
-                          var top = Math.round(ui.position.top);
-                          var position = { left : left, top : top };
-                          var sticky = tsp.lookups.sticky_from(ui.helper);
-                          sticky.update_position(position);
-                        },
-    update_surface_name: function(value, settings){
-                           var surface = tsp.lookups.current_surface();
-                           surface.update_name(value);
-                           return value;
-                       }
-  };
 
   var tsp = {};
-  tsp.handlers = handlers;
-  tsp.lookups = lookups;
-  tsp.builders = builders;
+  TSP.lookups = lookups;
+  TSP.builders = builders;
+  $.extend(tsp, TSP);
   return tsp; 
 };
--- spec/javascripts/tsp_spec.js
+++ spec/javascripts/tsp_spec.js
@@ -1,5 +1,6 @@
 require('spec_helper.js', {onload: function(){
     rails_require('tsp');
+    rails_require('tsp.handlers');
 }});
 
 var Helpers = {};
@@ -15,6 +16,7 @@ Helpers.sticky_div = function(){
 Helpers.get_surface_selector = function(){
   return '#surface';
 };
+
 
 Helpers.get_stickies_selector = function(){
   return "#stickies";
@@ -47,76 +49,6 @@ Screw.Unit(function(){
     tsp  = TSP.get();
     });
 
-  describe("tsp.handlers", function(){
-
-    describe("#destroy_sticky", function(){
-
-      it("calls destroy on sticky", function(){
-        var delete_div = {};
-        mock_sticky(tsp).should_receive('destroy').exactly(1);
-        tsp.handlers.destroy_sticky.apply(delete_div, []);
-      });
-    });
-
-    describe("#create_sticky", function(){
-      it("places created sticky on #stickies", function(){
-        tsp.handlers.create_sticky();
-      });
-    });
-
-    describe("#update_sticky_position", function(){
-      it("calls update_position on sticky", function(){
-        var ui_obj = { position : {
-                      left : 15, top: 100 },
-                      helper : get_sticky_div()};
-        var event_obj = 'unused';
-
-        mock_sticky(tsp).should_receive('update_position').exactly(1).with_arguments({left : 15, top : 100});
-        tsp.handlers.update_sticky_position(event_obj, ui_obj);
-      });
-    });
-
-    describe("#update_sticky_text", function(){
-      var sticky;
-      before(function(){
-        sticky = mock_sticky(tsp);
-        });
-      it("returns the value passed in", function(){
-        var value = tsp.handlers.update_sticky_text.apply(get_editable_div(), ['new text', null]);
-        expect(value).to(equal, "new text");
-      });
-
-      it("updates the sticky value with the passed value", function(){
-        sticky.should_receive('update_content').with_arguments('new content').exactly(1);
-
-        tsp.handlers.update_sticky_text.apply(get_editable_div(), ['new content', null]);
-      });
-    });
-
-    describe("#update_surface_name", function(){
-      var mock_surface = function(tsp){
-      var surface = mock();
-      stub(tsp.lookups, 'current_surface').and_return(surface);
-      stub(surface, 'update_name');
-      return surface;
-      };
-
-      var surface;
-      before(function(){
-        surface = mock_surface(tsp);
-        });
-      it("returns the value passed in", function(){
-        var value = tsp.handlers.update_surface_name.apply(get_editable_div(), ['new name', null]);
-        expect(value).to(equal, "new name");
-      });
-
-      it("updates the surface value with the passed value", function(){
-        surface.should_receive('update_name').with_arguments('this name').exactly(1);
-
-        tsp.handlers.update_surface_name.apply(get_editable_div(), ['this name', null]);
-      });
-    });
-  });
 
   describe("tsp.builders", function(){
     describe("#create_sticky", function(){
