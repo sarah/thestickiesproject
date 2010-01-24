3c0c3726862d358c46451d27e903aa7f35a6434b
Sarah Gray & Corey Haines <sarahg.gray+coreyhaines@gmail.com>
2010-01-24 16:27:18 -0600

    Create isolation for the TSP objects

--- public/javascripts/tsp.handlers.js
+++ public/javascripts/tsp.handlers.js
@@ -1,26 +1,30 @@
-TSP.handlers = {
-create_sticky: function() {
-                 TSP.builders.create_sticky();
-               },
-destroy_sticky: function(){
-                  var sticky = TSP.lookups.sticky_from(this);
-                  sticky.destroy();
-                },
-update_sticky_text: function(value, settings){
+TSP.handlers = (function() {
+  return {
+    create_sticky: function() {
+                     TSP.builders.create_sticky();
+                   },
+    destroy_sticky: function(){
                       var sticky = TSP.lookups.sticky_from(this);
-                      sticky.update_content(value);
-                      return value;
-                    },
-update_sticky_position: function(event, ui){
-                          var left = Math.round(ui.position.left);
-                          var top = Math.round(ui.position.top);
-                          var position = { left : left, top : top };
-                          var sticky = TSP.lookups.sticky_from(ui.helper);
-                          sticky.update_position(position);
-                        },
-update_surface_name: function(value, settings){
-                       var surface = TSP.lookups.current_surface();
-                       surface.update_name(value);
-                       return value;
-                     }
-};
+                      sticky.destroy();
+                   },
+    update_sticky_text: function(value, settings){
+                          var sticky = TSP.lookups.sticky_from(this);
+                          sticky.update_content(value);
+                          return value;
+                   },
+    update_sticky_position: function(event, ui){
+                              var left = Math.round(ui.position.left);
+                              var top = Math.round(ui.position.top);
+                              var position = { left : left, top : top };
+                              var sticky = TSP.lookups.sticky_from(ui.helper);
+                              sticky.update_position(position);
+                   },
+    update_surface_name: function(value, settings){
+                           var surface = TSP.lookups.current_surface();
+                           surface.update_name(value);
+                           return value;
+                   }
+  };
+})();
+
+
