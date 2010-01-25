57b5e23c9b5af303af64f62e59f89760990c4e1f
Sarah Gray & Corey Haines <sarahg.gray+coreyhaines@gmail.com>
2010-01-25 12:36:19 -0600

    JSLint cleanup for stickies.js
    - remove strict whitespace option

--- public/javascripts/stickies.js
+++ public/javascripts/stickies.js
@@ -1,5 +1,4 @@
-function hookup_sticky_new(){
+$(document).ready(function(){
   var handler = TSP.get().handlers.create_sticky;
   $('#new_sticky_button').click(handler);
-}
-$(document).ready(hookup_sticky_new);
+});
