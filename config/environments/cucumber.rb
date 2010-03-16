2c8a9695beadff4544d1c6448ef75cc5f0ee24df
Sarah Gray & Corey Haines <sarahg.gray+coreyhaines@gmail.com>
2010-03-16 14:40:22 +0000

    Cleanup config for cucumber

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -20,8 +20,8 @@ config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 config.action_mailer.default_url_options = { :host => "stickies.local" }
 
-config.gem 'cucumber',    :lib => false,        :version => '>=0.4.4' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber'))
-config.gem 'webrat',      :lib => false,        :version => '>=0.5.3' unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
-config.gem 'rspec',       :lib => false,        :version => '>=1.2.9' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
-config.gem 'rspec-rails', :lib => false,        :version => '>=1.2.9' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+config.gem 'cucumber',    :lib => false,        :version => '>=0.4.4'
+config.gem 'webrat',      :lib => false,        :version => '>=0.5.3'
+config.gem 'rspec',       :lib => false,        :version => '>=1.2.9'
+config.gem 'rspec-rails', :lib => false,        :version => '>=1.2.9'
 
