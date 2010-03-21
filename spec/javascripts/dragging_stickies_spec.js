65ba2c49e436874fcc93cf50e00cf4963a382f39
Corey Haines <coreyhaines@gmail.com>
2010-03-21 18:32:34 -0500

    Spec: Calling moveMyNeighbors on MoveVisitor

--- spec/javascripts/dragging_stickies_spec.js
+++ spec/javascripts/dragging_stickies_spec.js
@@ -1,10 +1,30 @@
 require('spec_helper.js', {onload: function(){
 }});
+var createSticky = function(createMovingVisitor){
+  var sticky = {};
+  sticky.youAreBeingDragged = function(dx, dy) {
+   createMovingVisitor().moveMyNeighbors(dx,dy); 
+  };
 
+  return sticky;
+};
 Screw.Unit(function(){
-  describe("DraggingStickies", function(){
-    it("does something", function(){
-      expect("hello").to(equal, "hello");
+  describe("Dragging a sticky", function(){
+    it("tells the MoveVisitor to move neighbors", function(){
+      var __called_moveMyNeighbors = false;
+      var movingVisitor = {
+        moveMyNeighbors: function() { __args_moveMyNeighbors = arguments; __called_moveMyNeighbors = true; }
+      };
+      var createMovingVisitor = function() {  return movingVisitor; };
+      var sticky = createSticky(createMovingVisitor);
+
+      var dx = 10;
+      var dy = 100;
+      sticky.youAreBeingDragged(dx, dy);
+
+      expect(__called_moveMyNeighbors).to(equal, true);
+      expect(__args_moveMyNeighbors[0]).to(equal, dx);
+      expect(__args_moveMyNeighbors[1]).to(equal, dy);
     });
   });
 });
