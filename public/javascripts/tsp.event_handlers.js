38da2bda21e6e27748080c616037d9b487959c98
Corey Haines & Sarah Gray <coreyhaines+stickies@gmail.com>
2010-04-11 18:53:10 -0500

    Extract sticky event handlers to stickies namespace

--- public/javascripts/tsp.event_handlers.js
+++ public/javascripts/tsp.event_handlers.js
@@ -2,25 +2,29 @@
 /*global TSP*/
 TSP.event_handlers = (function() {
   var tsp = TSP.get();
-  return {
-    create_sticky: function() {
-                     tsp.builders.create_sticky();
-                   },
-    destroy_sticky: function(){
+  var sticky_actions = {
+    destroy: function(){
                       var actions = tsp.lookups.actions_on(this);
                       actions.destroy();
                    },
-    update_sticky_text: function(new_content, settings){
+    update_text: function(new_content, settings){
                       var actions = tsp.lookups.actions_on(this);
                       actions.update_content(new_content);
                       return new_content;
                    },
-    update_sticky_position: function(event, ui){
+    update_position: function(event, ui){
                       var left = Math.round(ui.offset.left);
                       var top = Math.round(ui.offset.top);
                       var position = { left : left, top : top };
                       var actions = tsp.lookups.actions_on(ui.helper);
                       actions.update_position(position);
+                   }
+  };
+  var surface_actions = {};
+  return {
+    stickies: sticky_actions,
+    create_sticky: function() {
+                     tsp.builders.create_sticky();
                    },
     update_surface_name: function(value, settings){
                            var surface = tsp.lookups.current_surface();
--- spec/javascripts/tsp.event_handlers_spec.js
+++ spec/javascripts/tsp.event_handlers_spec.js
@@ -21,12 +21,12 @@ Screw.Unit(function(){
 
   describe("tsp.event_handlers", function(){
 
-    describe("#destroy_sticky", function(){
+    describe("stickies#destroy", function(){
 
       it("calls destroy on sticky", function(){
         var delete_div = {};
         mock_sticky(tsp).should_receive('destroy').exactly(1);
-        tsp.event_handlers.destroy_sticky.apply(delete_div, []);
+        tsp.event_handlers.stickies.destroy.apply(delete_div, []);
       });
     });
 
@@ -36,32 +36,32 @@ Screw.Unit(function(){
       });
     });
 
-    describe("#update_sticky_position", function(){
+    describe("stickies#update_position", function(){
       it("calls update_position on sticky", function(){
-        var ui_obj = { position : {
+        var ui_obj = { offset : {
                       left : 15, top: 100 },
                       helper : get_sticky_div()};
         var event_obj = 'unused';
 
         mock_sticky(tsp).should_receive('update_position').exactly(1).with_arguments({left : 15, top : 100});
-        tsp.event_handlers.update_sticky_position(event_obj, ui_obj);
+        tsp.event_handlers.stickies.update_position(event_obj, ui_obj);
       });
     });
 
-    describe("#update_sticky_text", function(){
+    describe("stickies#update_text", function(){
       var sticky;
       before(function(){
         sticky = mock_sticky(tsp);
         });
       it("returns the value passed in", function(){
-        var value = tsp.event_handlers.update_sticky_text.apply(get_editable_div(), ['new text', null]);
+        var value = tsp.event_handlers.stickies.update_text.apply(get_editable_div(), ['new text', null]);
         expect(value).to(equal, "new text");
       });
 
       it("updates the sticky value with the passed value", function(){
         sticky.should_receive('update_content').with_arguments('new content').exactly(1);
 
-        tsp.event_handlers.update_sticky_text.apply(get_editable_div(), ['new content', null]);
+        tsp.event_handlers.stickies.update_text.apply(get_editable_div(), ['new content', null]);
       });
     });
 
